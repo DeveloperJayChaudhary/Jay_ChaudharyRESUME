91a0dc94bf02e98c13d3bac990ff852d20aed9d8
Jay Chaudhary <100920819+DeveloperJayChaudhary@users.noreply.github.com>
2024-04-06 14:22:28 +0530

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,24 +1,82 @@
 import streamlit as st
-from st_functions import st_button, load_css
-from PIL import Image
+import pandas as pd 
+from textblob import TextBlob
+from cleantext.sklearn import CleanTransformer
+from nltk.corpus import stopwords
 
-load_css()
+st.header("Sentiment Analysis",)
+st.write('''#### Subjectivity --> 0 to 1 \n
+- Towards 0 means universal fact \n
+- Towards 1 means fact may vary from person to person or time to time or place to place \n''')
+with st.expander("Analyze Text"):
+    text=st.text_input("Text Here: ")
+    if text:
+        blob=TextBlob(text)
+        if round(blob.sentiment.polarity,2)>0.25:
+                st.write("Postive")
+                st.write("Subjective: ",round(blob.sentiment.subjectivity,2))
+        elif round(blob.sentiment.polarity,2)<-0.25:
+            st.write("Negative")
+            st.write("Subjective: ",round(blob.sentiment.subjectivity,2))
+        else:
+            st.write("Neutral")
+            st.write("Subjective: ",round(blob.sentiment.subjectivity,2))
 
-st.write("[![Star](https://img.shields.io/github/stars/dataprofessor/links.svg?logo=github&style=social)](https://gitHub.com/dataprofessor/links)")
+        
+        # st.write("Polarity: ",round(blob.sentiment.polarity,2))
+        # st.write("Subjective: ",round(blob.sentiment.subjectivity,2))
+    
+    pre = st.text_input("Clean Text: ") 
+    sw=stopwords.words('english')
+    sw.remove("not")
+    sw.remove("isn't")
+    sw.remove("aren't")
+    sw.remove("didn't")
+    sw.remove("haven't")
+    sw.remove("don't")
+    sw.remove("hasn't")
 
-col1, col2, col3 = st.columns(3)
-col2.image(Image.open('dp.png'))
+    if pre:
+        words=pre.split()
+        newdoc=''
+        for word in words:
+            if word not in sw:
+                newdoc=newdoc+word+" "
+        cleaner = CleanTransformer(no_punct=True, lower=True,no_numbers=True,replace_with_number=" ",
+                            no_urls=True,replace_with_url=" ",no_emails=True,replace_with_email=" ",no_digits=True,replace_with_digit=" ",no_emoji=True,no_currency_symbols=True,replace_with_currency_symbol=" ")
+        cleantext=cleaner.transform([newdoc])
+        st.write(cleantext[0])
+with st.expander("Analyze CSV"):
+    upl=st.file_uploader("Upload File")
+    def score(pre):
+        blob1=TextBlob(pre)
+        return blob1.sentiment.polarity
+    
+    def Analyze(x):
+        if x>=0.5:
+            return "Postive"
+        elif x<= -0.5:
+            return "Negative"
+        else: return "Neutral"    
+        
 
-st.header('Chanin Nantasenamat, Ph.D.')
+    if upl:
+        df=pd.read_csv(upl)
+        # del df['Unnamed : 0']
+        df['score']=df['tweets'].apply(score)
+        df['Analysis']=df["score"].apply(Analyze)
+        st.write(df.head(10))
+    
+        @st.cache_data
+        def convert_df(df):
+            # IMPORTANT : Cache the conversion to prevent computation on every rerun
+            return df.to_csv().encode('utf-8')
 
-st.info('Developer Advocate, Content Creator and ex-Professor with an interest in Data Science and Bioinformatics')
+        csv = convert_df(df)
 
-icon_size = 20
-
-st_button('youtube', 'https://youtube.com/dataprofessor', 'Data Professor YouTube channel', icon_size)
-st_button('youtube', 'https://youtube.com/codingprofessor', 'Coding Professor YouTube channel', icon_size)
-st_button('medium', 'https://data-professor.medium.com/', 'Read my Blogs', icon_size)
-st_button('twitter', 'https://twitter.com/thedataprof/', 'Follow me on Twitter', icon_size)
-st_button('linkedin', 'https://www.linkedin.com/in/chanin-nantasenamat/', 'Follow me on LinkedIn', icon_size)
-st_button('newsletter', 'https://sendfox.com/dataprofessor/', 'Sign up for my Newsletter', icon_size)
-st_button('cup', 'https://www.buymeacoffee.com/dataprofessor/', 'Buy me a Coffee', icon_size)
+        st.download_button(
+            label="Download data as CSV",
+            data=csv,
+            file_name='sentiment.csv',
+            mime='text/csv',
+        )
